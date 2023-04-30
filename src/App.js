90e3f4613b91137759157ca88364a85b0f7e2668
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-04-30 10:24:53 -0400

    initila commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import mountain from './mountain.webp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={mountain} className="App-logo" alt="mountain image" />
+        
       </header>
     </div>
   );
